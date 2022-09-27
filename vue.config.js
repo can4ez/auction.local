d89750484aa7ff177318dab198cd754afdd0a86d
Alex <schein.sascha2013@mail.ru>
2022-09-27 22:54:36 +0500

    Настроил работу через домен даже в dev mode

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
+  devServer: {
+    // 'auto' | 'all' [string] here
+    allowedHosts: 'all',
+  },
   transpileDependencies: true,
-  publicPath: './',
-  css: {
-    loaderOptions: {
-      // по умолчанию опция `sass` будет применяться к обоим синтаксисам
-      // потому что синтаксис `scss` по сути также обрабатывается sass-loader
-      // но при настройке опции `prependData` синтаксис `scss` требует точку с запятой
-      // в конце оператора, в то время как для `sass` точки с запятой не требуется
-      // в этом случае синтаксис `scss` можно настроить отдельно с помощью опции `scss`
-      scss: {
-        // additionalData: '@import "~@/scss/app.scss";'
-      }
-    }
-  }
+  publicPath: './'
 })
